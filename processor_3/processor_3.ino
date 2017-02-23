0f312383c027a2135fe91ad255e59a849df6648d
Tad Miller <tad@quantumedge.io>
2017-02-23 00:36:10 -0500

    Added coords object for x, y tracking

--- processor_3/processor_3.ino
+++ processor_3/processor_3.ino
@@ -4,13 +4,39 @@
 #include <SPI.h>
 #include <EEPROM.h>
 
-// KEYPAD CODE
-const byte ROWS = 4; //four rows
-const byte COLS = 3; //three columns
-char keys[ROWS][COLS] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'} };
-byte rowPins[ROWS] = {8, 7, 6, 5}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {4, 3, 2}; //connect to the column pinouts of the keypad
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+class Coords
+{
+    private:
+        int x;
+        int y;
+
+    public:
+        Coords(int x, int y)
+        {
+            this -> x = x;
+            this -> y = y;
+        }
+
+        int getX()
+        {
+            return x;
+        }
+
+        int getY()
+        {
+            return y;
+        }
+};
+
+//
+
+/*******************/
+/* KEYPAD CODE     */
+/*******************/
+char keys[4][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'} };
+byte rowPins[4] = {8, 7, 6, 5}; //connect to the row pinouts of the keypad
+byte colPins[3] = {4, 3, 2}; //connect to the column pinouts of the keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, 4, 3);
 
 // RGB 8x8 PROCESSOR CODE
 int bits[8] = {128, 64, 32, 16, 8, 4, 2, 1};
@@ -146,6 +172,8 @@ void placeShips()
     while (1)
         placeShip(1);
 }
+
+
 
 bool isValidSpot(int row, int col)
 {
